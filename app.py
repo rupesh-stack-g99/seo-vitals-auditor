8d643d7635d1d33575f52b0bee7640709edb156c
rupesh-stack-g99 <rupesh.ghodpage@growth99.net>
2026-06-23 09:04:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 
 # --- CONFIGURATION & UI SETUP ---
 st.set_page_config(
-    page_title="SiteVitals Pro | Growth99", 
+    page_title="PageSpeed Auditor| Growth99", 
     page_icon="🚀", 
     layout="wide"
 )
